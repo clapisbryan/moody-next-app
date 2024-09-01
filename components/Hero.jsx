c75e4905962923d55231e89d902613ad4bc91418
unknown <clapisbryan@gmail.com>
2024-09-01 15:48:13 +0800

    Added apos for single quote

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,13 +8,15 @@ export default function Hero() {
     return (
         <div className="py-4 md:py-10 flex flex-col gap-4 sm:gap-5">
             <div className="text-center w-full max-w-[900px] mx-auto">
-                <h1 className={'text-3xl sm:text-4xl md:text-5xl' + fugaz.className}>
+                <h1 className={`text-3xl sm:text-4xl md:text-5xl ${fugaz.className}`}>
                     <span className="text-gradient">Moody </span>
-                    <span>helps you track you're </span>
+                    <span>helps you track you&apos;re </span>
                     <span className="text-gradient">daily mood!</span>
                 </h1>
             </div>
-            <p className="text-base sm:text-lg md:text-xl text-center w-full mx-auto max-w-[600px]">Create your mood record and see how you feel on <span className="font-semibold">every day or every year.</span></p>
+            <p className="text-base sm:text-lg md:text-xl text-center w-full mx-auto max-w-[600px]">
+                Create your mood record and see how you feel on <span className="font-semibold">every day or every year.</span>
+            </p>
             <div className="grid grid-cols-2 gap-4 w-fit mx-auto">
                 <Button text="Sign Up" outline />
                 <Button text="Login" />
--- components/Login.jsx
+++ components/Login.jsx
@@ -9,7 +9,7 @@ export default function Login() {
         <>
             <div className="flex flex-col flex-1 justify-center items-center gap-4">
                 <h3 className={`text-xl sm:text-2xl md:text-3xl ${fugaz.className}`}>Log In / Register</h3>
-                <p>You're one step away!</p>
+                <p>You&apos;re one step away!</p>
                 <div className="max-w-[400px] w-full mx-auto">
                     <div className="mb-4">
                         <input
@@ -25,7 +25,7 @@ export default function Login() {
                     </div>
                     <Button text="Submit" full />
                     <div className="my-3 text-center">
-                        <p>Don't have an account? <span className="text-primary">Sign Up</span></p>
+                        <p>Don&apos;t have an account? <span className="text-primary">Sign Up</span></p>
                     </div>
                 </div>
             </div>
